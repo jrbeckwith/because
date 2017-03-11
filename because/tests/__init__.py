d9a77e6470764bc14953bde93f884561cde4d0a1
Sasha Hart <harts@boundlessgeo.com>
2017-03-11 14:52:53 -0600

    setup: testing
    An empty subpackage `because.tests` is created which will contain all the
    Python modules defining unit tests.
    
    The `pytest` package provides both the `py.test` script that is used to
    discover and run unit tests, and also test framework helpers for use inside
    test code, like `pytest.raises()`. `pytest` expects to see a directory named
    `tests/` containing files named `test_*.py`; inside these files, it looks for
    e.g. `class TestSomething` or `def test_something`. Unlike unittest, no
    subclassing is required, and you can use plain `assert whatever` instead of
    special `self.assertWhatever()` methods.
    
    `pytest-qt` is an addon for the py.test runner which may eventually help with
    qt testing.
    
    `coverage` instruments test runs to collect data on which segments of python
    files have been run or not during tests. Then it generates reports (e.g. in
    HTML) on the overall test coverage percentage and which code is not yet
    exercised by tests.
    
    `.coveragerc` contains settings for the `coverage` tool. In this case, the key
    bits are the ones involved in combining results across tox environments and
    enabling branch coverage.
    
    `tox` is used to orchestrate test running. it automatically creates virtualenvs
    for different environments (e.g. py2-qt4, py3-qt5) and runs tests in them to
    check cross-version compatibility. `coverage` is used to aggregate results
    across environments for accurate coverage even over e.g. py2 vs. py3
    conditionals.
    
    `tox.ini` is the configuration file for tox, specifying which environments are
    to be run, how they can be set up, and how to invoke the tests (in this case
    using `coverage` and `py.test`).
    
    `test_requirements.txt` is ad hoc for this project, as a way to specify test
    dependencies in one place (e.g. pytest, coverage). This file is currently only
    consumed by tox.ini.
    
    `.gitignore` is changed to prevent git from noticing the artifacts of running
    unit tests and collecting coverage data.

---
+++ because/tests/__init__.py
@@ -0,0 +1,6 @@
+"""Tests for because.
+
+The tests are included inside the package so that they can be run in any
+environment where because is failing, allowing us to blame the environment
+instead of our code if that environment is producing unexpected test failures.
+"""
