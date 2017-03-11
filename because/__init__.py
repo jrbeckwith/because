836d79104b80da6ffd1bddadb6701dc51e81dca9
Sasha Hart <harts@boundlessgeo.com>
2017-03-11 15:05:21 -0600

    code: expose because.Because
    expose Frontend from __init__.py as Because for examples etc.

--- because/__init__.py
+++ because/__init__.py
@@ -3,3 +3,9 @@
 The because package contains reusable implementations which can help you write
 Python scripts, QGIS plugins, etc.
 """
+from . frontend import Frontend
+
+
+class Because(Frontend):
+    """High-level entry point for quickstart docs and examples.
+    """
