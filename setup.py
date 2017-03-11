d0caf2b8e68b20a85455e4ce9103854fa86e071e
Sasha Hart <harts@boundlessgeo.com>
2017-03-11 14:10:09 -0600

    setup: package metadata
    in setup.py call to setup():
        name
        version
        description
        maintainer
        maintainer_email
        url

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 
 setup(
+    name="because",
+    version="0.0.0",
+    description="Python API for Boundless Services",
+    maintainer="Sasha Hart",
+    maintainer_email="harts@boundlessgeo.com",
+    url="https://github.com/harts-boundless/because",
     packages=[
     ],
     install_requires=[
