592af967fc706102ee9ddce04a934f06b2298c1d
jrbeckwith <jbeckwith@boundlessgeo.com>
2017-10-12 14:39:51 -0500

    Update headers.py

--- because/services/headers.py
+++ because/services/headers.py
@@ -7,6 +7,7 @@ DEFAULT_HEADERS = Headers.coerce({
     b"User-Agent": b"Because",
     b"Accept": b"application/json",
     b"Accept-Charset": b"utf-8",
+    b"X-Api-Version": b"0.1",
 
     # This is not always true because there's not always a body, right?
     # b"Content-Type": b"application/json",
